5945cffeba42174384d7028a8134bccb57b6e4d5
Djib <issoufoudjib@gmail.com>
2019-04-11 16:35:56 +0100

    Liaison de l'appli avec l'API

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -25,7 +25,31 @@ export class LoginPage {
   // Attempt to login in through our User service
   connection(form: NgForm) {
     if (form.valid) {
-      
+      var api = this.api;
+      var data = new FormData;
+      var navCtrl = this.navCtrl;
+      var toast = this.toastCtrl;
+      data.append("mode", "login");
+      data.append("tel", this.login.tel);
+      data.append("mdp", this.login.password);
+      const loader = this.loadingCtrl.create({
+        content: "Vérification...",
+        duration: 3000
+      });
+      loader.present();
+      api.ajaxPost(this.url, data, function(rep){
+        if (rep.success) {
+          api.user = rep.result;
+          navCtrl.setRoot(HomePage);
+        } else {
+          let test = toast.create({
+            message: rep.message,
+            duration: 3000,
+            position: 'top'
+          });
+          test.present();
+        }
+      });
 
     } else {
       this.submitted = true;
